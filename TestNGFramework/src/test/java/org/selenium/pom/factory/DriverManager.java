e665fc06b0c55d7489705f6de305b8a1df41ac1c
Moulali Syed <syedmoulali1997@gmail.com>
2022-05-22 08:57:51 +0530

    WebDriverManager

--- TestNGFramework/src/test/java/org/selenium/pom/factory/DriverManager.java
+++ TestNGFramework/src/test/java/org/selenium/pom/factory/DriverManager.java
@@ -13,6 +13,7 @@ public class DriverManager {
 	public WebDriver initializeDriver() {
 
 		WebDriverManager.chromedriver().setup();
+		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
